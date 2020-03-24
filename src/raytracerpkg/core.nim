b260358aca9cdab73e8b9a6fd0f7152b8cedf700
Carlos Soria <csoria@cultome.io>
2020-03-24 01:49:21 -0600

    Implement until magnitude

--- src/raytracerpkg/core.nim
+++ src/raytracerpkg/core.nim
@@ -1,9 +1,40 @@
+import math
+
 type
-  Point = tuple[x: float32, y: float32, z: float32, w: float32]
-  Vector = tuple[x: float32, y: float32, z: float32, w: float32]
+  Tuple* = tuple[x: float64, y: float64, z: float64, w: float64]
 
-proc point*(x, y, z: float32): Point =
-  (x: x, y: y, z: z, w: 1.0.float32)
+proc tp*(x, y, z, w: int32): Tuple =
+  (x: x.float64, y: y.float64, z: z.float64, w: w.float64)
 
-proc vector*(x, y, z: float32): Vector =
-  (x: x, y: y, z: z, w: 0.0.float32)
+proc tp*(x, y, z, w: float64): Tuple =
+  (x: x, y: y, z: z, w: w)
+
+proc point*(x, y, z: int32): Tuple =
+  (x: x.float64, y: y.float64, z: z.float64, w: 1.0.float64)
+
+proc point*(x, y, z: float64): Tuple =
+  (x: x, y: y, z: z, w: 1.0.float64)
+
+proc vector*(x, y, z: int32): Tuple =
+  (x: x.float64, y: y.float64, z: z.float64, w: 0.0.float64)
+
+proc vector*(x, y, z: float64): Tuple =
+  (x: x, y: y, z: z, w: 0.0.float64)
+
+proc `+`*(t1, t2: Tuple): Tuple =
+  (x: t1.x + t2.x, y: t1.y + t2.y, z: t1.z + t2.z, w: t1.w + t2.w)
+
+proc `-`*(t1, t2: Tuple): Tuple =
+  (x: t1.x - t2.x, y: t1.y - t2.y, z: t1.z - t2.z, w: t1.w - t2.w)
+
+proc `!`*(t: Tuple): Tuple =
+  (x: -t.x, y: -t.y, z: -t.z, w: -t.w)
+
+proc `*`*(t1: Tuple, scalar: float64): Tuple =
+  (x: t1.x * scalar, y: t1.y * scalar, z: t1.z * scalar, w: t1.w * scalar)
+
+proc `/`*(t1: Tuple, scalar: float64): Tuple =
+  (x: t1.x / scalar, y: t1.y / scalar, z: t1.z / scalar, w: t1.w / scalar)
+
+proc magnitude*(t: Tuple): float64 =
+  sqrt(pow(t.x, 2) + pow(t.y, 2) + pow(t.z, 2) + pow(t.w, 2))
--- tests/testtuples.nim
+++ tests/testtuples.nim
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 import raytracerpkg/core
 
@@ -11,8 +12,6 @@ suite "tuples features":
     check(p.z - 3.1 < 0.001)
     check(p.w - 1.0 < 0.001)
 
-    check($type(p) == "Point")
-
   test "a tuple with w=0 is a vector":
     var v = vector(4.3, -4.2, 3.1)
 
@@ -21,4 +20,83 @@ suite "tuples features":
     check(v.z - 3.1 < 0.001)
     check(v.w - 0.0 < 0.001)
 
-    check($type(v) == "Vector")
+  test "adding two tuples":
+    var
+      p = point(3.0, -2.0, 5.0)
+      v = vector(-2.0, 3.0, 1.0)
+
+    check(p + v == (1.0, 1.0, 6.0, 1.0))
+
+  test "substracting two points":
+    var
+      p1 = point(3.0, 2.0, 1.0)
+      p2 = point(5.0, 6.0, 7.0)
+
+    check(p1 - p2 == vector(-2.0, -4.0, -6.0))
+
+  test "substracting a vector from a point":
+    var
+      p = point(3, 2, 1)
+      v = vector(5, 6, 7)
+
+    check(p - v == point(-2, -4, -6))
+
+  test "substracting two vectors":
+    var
+      v1 = vector(3, 2, 1)
+      v2 = vector(5, 6, 7)
+
+    check(v1 - v2 == vector(-2, -4, -6))
+
+  test "substracting a vector from a zero vector":
+    var
+      v1 = vector(1, -2, 3)
+      vz = vector(0, 0, 0)
+
+    check(vz - v1 == vector(-1, 2, -3))
+
+  test "negate tuple":
+    var t: Tuple = tp(1, -2, 3, -4)
+
+    check(!t == tp(-1, 2, -3, 4))
+
+  test "multiply a tuple by a scalar":
+    var t: Tuple = tp(1, -2, 3, -4)
+
+    check(t * 3.5 == tp(3.5, -7, 10.5, -14))
+
+  test "multiply a tuple by a fraction":
+    var t: Tuple = tp(1, -2, 3, -4)
+
+    check(t * 0.5 == tp(0.5, -1, 1.5, -2))
+
+  test "divide a tuple by a scalar":
+    var t: Tuple = tp(1, -2, 3, -4)
+
+    check(t / 2 == tp(0.5, -1, 1.5, -2))
+
+  test "computing the magnitude of vector (1, 0, 0)":
+    var v = vector(1, 0, 0)
+
+    check(v.magnitude == 1)
+
+  test "computing the magnitude of vector (0, 1, 0)":
+    var v = vector(0, 1, 0)
+
+    check(v.magnitude == 1)
+
+  test "computing the magnitude of vector (0, 0, 1)":
+    var v = vector(0, 0, 1)
+
+    check(v.magnitude == 1)
+
+  test "computing the magnitude of vector (1, 2, 3)":
+    var v = vector(1, 2, 3)
+
+    check(v.magnitude == sqrt(14.0))
+
+  test "computing the magnitude of vector (-1, -2, -3)":
+    var v = vector(-1, -2, -3)
+
+    check(v.magnitude == sqrt(14.0))
+
