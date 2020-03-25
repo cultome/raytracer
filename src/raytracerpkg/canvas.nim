c25dc567ccfdeea748266db4ec2c9ff0b1582ba9
Carlos Soria <csoria@cultome.io>
2020-03-24 18:15:40 -0600

    Implement write and read methods for canvas

--- src/raytracerpkg/canvas.nim
+++ src/raytracerpkg/canvas.nim
@@ -1,7 +1,29 @@
+import raytracerpkg/core
+
 type
-  Canvas* = object
+  Canvas* = ref object
     width*: int
     height*: int
+    pixels*: seq[seq[Color]]
 
 proc canvas*(width, height: int): Canvas =
-  Canvas(width: width, height: height)
+  var
+    pixels: seq[seq[Color]]
+    col: seq[Color]
+    color = color(0, 0, 0)
+
+  for x in 0..<width:
+    col = @[]
+
+    for y in 0..<height:
+      col.add(color)
+
+    pixels.add(col)
+
+  Canvas(width: width, height: height, pixels: pixels)
+
+proc writePixel*(c: Canvas, x, y: int, color: Color) =
+  c.pixels[x][y] = color
+
+proc pixelAt*(c: Canvas, x, y: int): Color =
+  c.pixels[x][y]
--- tests/tcanvas.nim
+++ tests/tcanvas.nim
@@ -1,6 +1,7 @@
 import unittest
 
 import raytracerpkg/canvas
+import raytracerpkg/core
 
 suite "canvas features":
   test "creating a canvas":
@@ -10,3 +11,11 @@ suite "canvas features":
     check(c.height == 20)
 
     # ... and every pixel is color(0, 0, 0)
+  test "write pixels to canvas":
+    var
+      c = canvas(10, 20)
+      r = color(1, 0, 0)
+
+    c.writePixel(2, 3, r)
+
+    check(c.pixelAt(2, 3) == r)
